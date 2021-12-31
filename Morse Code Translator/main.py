5521f5b32c18f17fb668879d41c805b0f65740e0
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2022-01-01 02:12:58 +0700

    upload file

---
+++ Morse Code Translator/main.py
@@ -0,0 +1,39 @@
+# Dari Huruf Alphabet ke Kode Morse
+enkrip = {
+    " " : "/",
+    "A" : ".-",
+    "B" : "-...",
+    "C" : "-.-.",
+    "D" : "-..",
+    "E" : ".",
+    "F" : "..-.",
+    "G" : "--.",
+    "H" : "....",
+    "I" : "..",
+    "J" : ".---",
+    "K" : "-.-",
+    "L" : ".-..",
+    "M" : "--",
+    "N" : "-.",
+    "O" : "---",
+    "P" : ".--.",
+    "Q" : "--.-",
+    "R" : ".-.",
+    "S" : "...",
+    "T" : "-",
+    "U" : "..-",
+    "V" : "...-",
+    "W" : ".--",
+    "X" : "-..-",
+    "Y" : "-.--",
+    "Z" : "--.."
+}
+
+# Dari Kode Morse ke Huruf Alphabet
+dekrip = {key:value for value,key in enkrip.items()}
+
+def main() :
+   pass
+
+if __name__ == "__main__" :
+    main()
