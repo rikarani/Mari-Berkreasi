0fde8c3fb5d1d026c1b923ec2a47fe722b677e1e
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2022-01-03 20:33:39 +0700

    Translate dari Huruf Alphabet ke Kode Morse

--- Morse Code Translator/main.py
+++ Morse Code Translator/main.py
@@ -47,11 +47,17 @@ dekrip = {key:value for value,key in enkrip.items()}
 
 def main() :
     kalimat = input().upper()
+    sandi = [] # List buat nyimpan hasil translate nya
 
     if kalimat.startswith(".") or kalimat.startswith("-") : # Kode Morse tuh kan awalnya kalo nda . ya -
         print("Ini Kode Morse")
+    
+    # Dari Huruf Alphabet ke Kode Morse
     else :
-        print("Ini Huruf Alphabet")
+        for kata in kalimat : # Setiap Alphabet yang ada di variabel kalimat disimpan di variabel kata, trus diulang
+            sandi.append(enkrip[kata])
+        
+        print("Kode Morse dari", kalimat , "Adalah" , " ".join(sandi))
 
 if __name__ == "__main__" :
     main()
